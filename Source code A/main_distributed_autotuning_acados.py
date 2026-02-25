48282727f07f0ba3b50380c21484a7645abecc9a
Wang Bingheng <70559054+BinghengNUS@users.noreply.github.com>
2026-02-25 18:37:07 +0800

    Update main_distributed_autotuning_acados.py

--- Source code A/main_distributed_autotuning_acados.py
+++ Source code A/main_distributed_autotuning_acados.py
@@ -577,6 +577,7 @@ if __name__ == '__main__':
                             norm_grad_i   = LA.norm(dlidp)
                             print('k_train=',k_train,'time step=',k,'loss_i=',loss_i,'norm_grad_i=',format(norm_grad_i,'.4f'))
                             loss_nn_i     = NN_Quad[i].myloss(NN_Quad[i](NN_input_quad[i]),dlidp) # This uses the current hyperparameters \theta_k. In theory, it should have used the historical hyperparameters.
+                            # We treat \theta_t [k:k+Ncl] as locally frozen during the gradient window. This avoids computing Ncl independent sensitivity trajectories.
                             optimizer     = torch.optim.Adam(NN_Quad[i].parameters(),lr=lr_nn) # This should be moved outside the training loop; however, it does not affect the training results.
                             NN_Quad[i].zero_grad()
                             loss_nn_i.backward()
